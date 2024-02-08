5fc425f404938a3f43151f039696a61d8f7ae385
Michal Kurzewski <kurzewski.m@gmail.com>
2024-02-08 17:36:56 +0000

    update

---
+++ storybook-content/assets/NavLinks.stories-Z6nrvMKh.js
@@ -0,0 +1,6 @@
+import"./index-IybTgENJ.js";import{N as e}from"./NavLinks-g-pyZqH3.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./jsx-runtime-CbCNBOXy.js";import"./index--dM_I6vX.js";import"./index-5bnWhD0C.js";import"./index-PMrafJYB.js";import"./Button-C3tTEqbI.js";import"./motion-81d63Fkm.js";const u={title:"Codeng/Molecules/Nav Links",component:e,tags:["autodocs"]},r={args:{loggedIn:!1,signOut:()=>{}}};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    loggedIn: false,
+    signOut: () => {}
+  }
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const f=["Primary"];export{r as Primary,f as __namedExportsOrder,u as default};
