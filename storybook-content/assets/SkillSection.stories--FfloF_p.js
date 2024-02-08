01ef7b19c613ba97014d29656c053576134f08bf
Michal Kurzewski <kurzewski.m@gmail.com>
2024-02-08 23:45:36 +0000

    update

---
+++ storybook-content/assets/SkillSection.stories--FfloF_p.js
@@ -0,0 +1,7 @@
+import{j as e}from"./jsx-runtime-CbCNBOXy.js";import{S as n,a as l,b as a}from"./net-eBQ_Ow1T.js";import"./index-IybTgENJ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./SkillItem-hVUz1Y59.js";import"./motion-81d63Fkm.js";const t={sectionTitle:"LOREM",description:e.jsxs("div",{className:"text-left tracking-wide indent-4",children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}),e.jsx("p",{children:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."})]}),skills:[{Icon:l,skillName:"Lorem1"},{Icon:a,skillName:"Lorem2"}]},g={title:"Codeng/Organisms/Skill Section",component:n,tags:["autodocs"]},i={args:{description:t.description,sectionTitle:t.sectionTitle,skills:t.skills}};var s,o,r;i.parameters={...i.parameters,docs:{...(s=i.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    description: mockSkills.description,
+    sectionTitle: mockSkills.sectionTitle,
+    skills: mockSkills.skills
+  }
+}`,...(r=(o=i.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const S=["Primary"];export{i as Primary,S as __namedExportsOrder,g as default};
--- storybook-content/assets/preview-rL-lgh68.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-sZK0dnCD.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-yqGmpFbL.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-yqGmpFbL.js","./iframe-sZK0dnCD.js","./index-IybTgENJ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-2YZTwWBn.js","./index-5bnWhD0C.js","./index-ogXoivrg.js","./extends-558kR2v1.js","./_baseIteratee-zmUDD9Da.js","./index-PPLHz8o0.js","./index-CLtXfnV-.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
